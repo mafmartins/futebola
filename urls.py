2e3b0201e21c155706a731cfb783509686ea0311
mafmartins <marcio.af.martins@gmail.com>
2017-10-16 17:51:59 +0200

    Adicionada página para gerar equipas com base na pontuação individual

--- urls.py
+++ urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # ex: /futebola/jogo/5/
     url(r'^jogo/(?P<jogo_id>[0-9]+)/$', views.jogo, name='jogo'),
     # ex: /futebola/jogo/5/
-    url(r'^jogador/(?P<jogador_id>[0-9]+)/$', views.jogador, name='jogador')
+    url(r'^jogador/(?P<jogador_id>[0-9]+)/$', views.jogador, name='jogador'),
+    # ex: /futebola/jogo/5/
+    url(r'^gerarequipas/', views.gerarEquipas, name='gerarEquipas')
 ]
--- views.py
+++ views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Jogador, Jogo, Ficha_de_jogo
 import datetime, json
 
@@ -67,3 +67,105 @@ def jogador(request, jogador_id):
     }
         
     return render(request, 'futebola/jogador.html', context)
+  
+def gerarEquipas(request):
+    lista_jogadores = Jogador.objects.order_by('nome')
+    
+    context = {
+        'lista_jogadores': lista_jogadores
+    }
+    
+    if request.method == 'GET':
+        return render(request, 'futebola/gerarEquipas.html', context)
+    else:            
+        ar = []
+        ints = []
+        for item in request.POST:
+            try:
+                obj = {item : int(request.POST[item])}
+                ar.append(obj)
+                ints.append(int(request.POST[item]))
+            except:
+                continue
+
+        vals1, vals2 = team(ints)
+
+        team1 = []
+        team2 = []
+
+        for x in vals1:
+          for y in request.POST:
+            try:
+              if x == int(request.POST[y]):
+                team1.append({y : request.POST[y]})
+            except:
+              continue
+        for x in vals2:
+          for y in request.POST:
+            try:
+              if x == int(request.POST[y]):
+                team2.append({y : request.POST[y]})
+            except:
+              continue
+        
+        equipa1 = []
+        equipa2 = []
+        sume1 = 0
+        sume2 = 0
+        
+        for x in team1:
+            equipa1.append(get_object_or_404(Jogador, jogador_id=int(x.keys()[0])))
+            sume1 += int(x.values()[0])
+            
+        for x in team2:
+            equipa2.append(get_object_or_404(Jogador, jogador_id=int(x.keys()[0])))
+            sume2 += int(x.values()[0])
+            
+        context = {
+            'equipa1': equipa1,
+            'equipa2': equipa2,
+            'somae1' : sume1,
+            'somae2' : sume2
+        }
+        
+        return render(request, 'futebola/verEquipasGeradas.html', context)
+    
+def team(t):
+    iterations = range(2, len(t)/2+1)
+
+    totalscore = sum(t)
+    halftotalscore = totalscore/2.0
+
+    oldmoves = {}
+    
+    rest = []
+
+    for p in t:
+        people_left = t[:]
+        people_left.remove(p)
+        oldmoves[p] = people_left
+
+    if iterations == []:
+        solution = min(map(lambda i: (abs(float(i)-halftotalscore), i), oldmoves.keys()))
+        return (solution[1], sum(oldmoves[solution[1]]), oldmoves[solution[1]])
+
+    for n in iterations:
+        newmoves = {}
+        for total, roster in oldmoves.iteritems():
+            for p in roster:
+                people_left = roster[:]
+                people_left.remove(p)
+                newtotal = total+p
+                if newtotal > halftotalscore: continue
+                newmoves[newtotal] = people_left
+        oldmoves = newmoves
+
+    solution = min(map(lambda i: (abs(float(i)-halftotalscore), i), oldmoves.keys()))
+    
+    for item in oldmoves[solution[1]]:
+        for obj in t:
+            if item == obj:
+                t.remove(obj)
+                break
+    #return (sum(oldmoves[solution[1]]), oldmoves[solution[1]], solution[1], t)
+    return (oldmoves[solution[1]], t)
